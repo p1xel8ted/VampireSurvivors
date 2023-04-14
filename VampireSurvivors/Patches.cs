1568d4622cc93fdbafbfcaf9074fbf596a41a6b0
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-15 08:46:56 +1000

    Initial commit

---
+++ VampireSurvivors/Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using Screen = UnityEngine.Device.Screen;
+
+namespace VampireSurvivors;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AspectRatioFitter), nameof(AspectRatioFitter.OnEnable))]
+    [HarmonyPatch(typeof(AspectRatioFitter), nameof(AspectRatioFitter.Start))]
+    public static void AspectRatioFitter_Patches(ref AspectRatioFitter __instance)
+    {
+        __instance.aspectRatio = Display.main.systemWidth / (float)Display.main.systemHeight;
+        var maxFramerate = Screen.resolutions.Where(x => x.width == Display.main.systemWidth && x.height == Display.main.systemHeight).Max(x => x.refreshRate);
+        Application.targetFrameRate = maxFramerate;
+    }
+    
+}
---
+++ VampireSurvivors/VampireSurvivors.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(VampireSurvivors.VampireSurvivors), "Vampire Survivors UltraWide Fixes", "0.0.1", "p1xel8ted")]
+[assembly: MelonGame("poncle", "VampireSurvivors")]
+
+namespace VampireSurvivors;
+
+public class VampireSurvivors : MelonMod
+{
+
+}
