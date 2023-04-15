e7373a4d8dca206b57e15567f9fda8f36962b9b7
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-15 16:37:03 +1000

    Refactor AspectRatioFitter_Patches to only set aspectRatio if _expandUI.Value is true.

--- VampireSurvivors/Patches.cs
+++ VampireSurvivors/Patches.cs
@@ -1,21 +1,21 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
-using Screen = UnityEngine.Device.Screen;
 
 namespace VampireSurvivors;
 
 [HarmonyPatch]
-public static class Patches
+public partial class VampireSurvivors
 {
     [HarmonyPostfix]
     [HarmonyPatch(typeof(AspectRatioFitter), nameof(AspectRatioFitter.OnEnable))]
     [HarmonyPatch(typeof(AspectRatioFitter), nameof(AspectRatioFitter.Start))]
     public static void AspectRatioFitter_Patches(ref AspectRatioFitter __instance)
     {
-        __instance.aspectRatio = Display.main.systemWidth / (float)Display.main.systemHeight;
-        var maxFramerate = Screen.resolutions.Where(x => x.width == Display.main.systemWidth && x.height == Display.main.systemHeight).Max(x => x.refreshRate);
-        Application.targetFrameRate = maxFramerate;
+        if (_expandUI.Value)
+        {
+            __instance.aspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
+        }
     }
-    
+
 }
