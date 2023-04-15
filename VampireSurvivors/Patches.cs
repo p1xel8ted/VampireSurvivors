31584c5a4588f03e6e462a2ac808ee2c2f2cb0b5
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-16 09:40:25 +1000

    Add Patches.cs file with Stage_EnemySpawnLocations Harmony patch method. Currently results in CTD

---
+++ VampireSurvivors/Patches.cs
@@ -0,0 +1,29 @@
+﻿// using HarmonyLib;
+// using Il2CppVampireSurvivors.Objects;
+// using UnityEngine;
+// using UnityEngine.Purchasing;
+//
+// namespace VampireSurvivors
+// {
+//     [HarmonyPatch]
+//     public static class Patches
+//     {
+//         [HarmonyPrefix]
+//         [HarmonyPatch(typeof(Stage), nameof(Stage.EnemySpawnLocations), MethodType.Getter)]
+//         public static bool Stage_EnemySpawnLocations(ref Il2CppSystem.Collections.Generic.List<Vector2> __result)
+//         {
+//             Il2CppSystem.Collections.Generic.List<Vector2> resultList = __result;
+//             int count = resultList.Count;
+//
+//             for (int index = 0; index < count; index++)
+//             {
+//                 Vector2 modifiedVector = new Vector2(-20, 0);
+//                 resultList.RemoveAt(index);
+//                 resultList.Insert(index, modifiedVector);
+//             }
+//
+//             __result = resultList;
+//             return false;
+//         }
+//     }
+// }
