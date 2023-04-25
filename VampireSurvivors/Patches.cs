8d9affe2559e1776058decd78eab1e651b9ad61e
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-25 23:07:07 +1000

    Add Harmony patches for Stage and GameOverPage in VampireSurvivors.

--- VampireSurvivors/Patches.cs
+++ VampireSurvivors/Patches.cs
@@ -1,29 +1,36 @@
-﻿// using HarmonyLib;
-// using Il2CppVampireSurvivors.Objects;
-// using UnityEngine;
-// using UnityEngine.Purchasing;
-//
-// namespace VampireSurvivors
-// {
-//     [HarmonyPatch]
-//     public static class Patches
-//     {
-//         [HarmonyPrefix]
-//         [HarmonyPatch(typeof(Stage), nameof(Stage.EnemySpawnLocations), MethodType.Getter)]
-//         public static bool Stage_EnemySpawnLocations(ref Il2CppSystem.Collections.Generic.List<Vector2> __result)
-//         {
-//             Il2CppSystem.Collections.Generic.List<Vector2> resultList = __result;
-//             int count = resultList.Count;
-//
-//             for (int index = 0; index < count; index++)
-//             {
-//                 Vector2 modifiedVector = new Vector2(-20, 0);
-//                 resultList.RemoveAt(index);
-//                 resultList.Insert(index, modifiedVector);
-//             }
-//
-//             __result = resultList;
-//             return false;
-//         }
-//     }
-// }
+﻿using HarmonyLib;
+using Il2CppVampireSurvivors.Data;
+using Il2CppVampireSurvivors.Objects;
+using Il2CppVampireSurvivors.UI;
+using MelonLoader;
+using UnityEngine;
+
+namespace VampireSurvivors;
+
+[HarmonyPatch]
+public partial class VampireSurvivors
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Stage), nameof(Stage.InitStage))]
+    public static void Stage_InitStage(StageType stageType)
+    {
+        CurrentType = stageType;
+        Melon<VampireSurvivors>.Logger.Warning($"Stage: {CurrentType.ToString()}");
+    }
+    
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameOverPage), nameof(GameOverPage.Start))]
+    [HarmonyPatch(typeof(GameOverPage), nameof(GameOverPage.OnShowStart))]
+    [HarmonyPatch(typeof(GameOverPage), nameof(GameOverPage.Construct))]
+    public static void GameOverPage_Start(ref GameOverPage __instance)
+    {
+        __instance._Background.preserveAspect = false;
+        var deathScreenMessage = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - Game Over/Container");
+        if (deathScreenMessage != null)
+        {
+            Melon<VampireSurvivors>.Logger.Warning($"Found Death Screen Message - Adjusting Position");
+            deathScreenMessage.transform.localPosition = new Vector3(4.2336f, -513.2808f, 0);
+        }
+    }
+}
